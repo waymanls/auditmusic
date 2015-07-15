b6965aa43bb2584483bfdb1540cbfb02b297d38c
waymanls <wayman.smith@gmail.com>
2015-07-15 16:53:05 -0400

    now calling querymusicbrainz module in new.py

--- new.py
+++ new.py
@@ -4,53 +4,52 @@
 import time
 import sys, parser, os, re
 import argparse
+import querymusicbrainz
 
-parser = argparse.ArgumentParser(description='Music Scanner')
-parser.add_argument('-d','--directory', help='Directory', required=True)
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description='Music Scanner')
+    parser.add_argument('-d','--directory', help='Directory', required=True)
+    args = parser.parse_args()
+    x = 0
+    L = []
+    badalbums = []
 
-
-#M = sys.argv[1]
-x = 0
-L = []
-badalbums = []
-
-
-for dirName,subdirList,fileList in os.walk(args.directory):
-    #Prune bad directories from list
-    if re.search(r'([Ss]ingles|lost\+found|System\ Volume\ Information|.*RECYCLE?)',dirName):
-        pass
-    else:
-        try:
-            # Separate out Artist Name from Album Title
-            em = re.search(r'^.*/(music|MUSIC|Music)/(.*)_-_(.*)/?',dirName)
-            # Prune off extra "/". if group 2 contains a "/" character, don't print
-            if re.search(r'/',em.group(2)):
-                pass
-            else:
-                #print em.group(1) ,"~~~", em.group(2)
-                for fname in fileList:
-                    # Get actual music files, not other files
-                    if re.search(r'\.(flac|wav|mp3|m4a|mp4|wma)',fname):
-                        L.append(fname)
-                        x = x+1
-                if x == 0:
+    for dirName,subdirList,fileList in os.walk(args.directory):
+        #Prune bad directories from list
+        if re.search(r'([Ss]ingles|lost\+found|System\ Volume\ Information|.*RECYCLE?)',dirName):
+            pass
+        else:
+            try:
+                # Separate out Artist Name from Album Title
+                em = re.search(r'^.*/(music|MUSIC|Music)/(.*)_-_(.*)/?',dirName)
+                # Prune off extra "/". if group 2 contains a "/" character, don't print
+                if re.search(r'/',em.group(2)):
                     pass
                 else:
-                    pass
-                    # Print out total files contained in Album
-                    #print x , "songs in", em.group(1) , em.group(2)
+                    #print em.group(1) ,"~~~", em.group(2)
+                    for fname in fileList:
+                        # Get actual music files, not other files
+                        if re.search(r'\.(flac|wav|mp3|m4a|mp4|wma)',fname):
+                            L.append(fname)
+                            x = x+1
+                    if x == 0:
+                        pass
+                    else:
+                        pass
+                        # Print out total files contained in Album
+                        #print x , "songs in", em.group(1) , em.group(2)
+                        # Do you want to print this data to stdout or write it to a file? 
+                        print x , "songs in", dirName
+                        print em.group(2),em.group(3)
+                        # Function that compares my albums to musicBrainz goes here!
+                        querymusicbrainz.gather(em.group(2),em.group(3))
+                    L = []
+                    x = 0
+            except AttributeError:
+                print "Cannot parse ", dirName
+                badalbums.append(dirName)
 
-                    # Do you want to print this data to stdout or write it to a file? 
-                    print x , "songs in", dirName
-                    # Function that compares my albums to musicBrainz goes here!
-                    #
-                    # End
-                L = []
-                x = 0
-        except AttributeError:
-            print "Cannot parse ", dirName
-            badalbums.append(dirName)
-
+if __name__ == "__main__":
+    main()
 #This is a comment to test branches
 # vim:ts=4:expandtab:
--- querymusicbrainz.py
+++ querymusicbrainz.py
@@ -4,28 +4,26 @@ import musicbrainzngs
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description='Audit Music collection')
-parser.add_argument('-a','--artist', help='Artist Name', required=True)
-parser.add_argument('-t','--title', help='Album Title', required=True)
-args = parser.parse_args()
-
-
+#parser = argparse.ArgumentParser(description='Audit Music collection')
+#parser.add_argument('-a','--artist', help='Artist Name', required=True)
+#parser.add_argument('-t','--title', help='Album Title', required=True)
+#args = parser.parse_args()
 musicbrainzngs.set_useragent(
     "auditmusic musicbrainz query",
     "0.1",
     "https://github.com/luckyleftie/auditmusic/",
 )
-
 # Add getopts thing. So you can run it like this
 # querymusicbrainz.py -a <ARTIST> -t <ALBUM TITLE>
-
-result = musicbrainzngs.search_releases(artist=args.artist, release=args.title)
-for item in result['release-list']:
-    if (item['artist-credit'][0]['artist']['name'] == args.artist and item['title'] == args.title):
-        actual = item['id']
-        foo = musicbrainzngs.get_release_by_id(actual,includes=['recordings','release-groups'])
-        print foo
-#print "Positions: ",foo['release']['medium-list'][0]['track-list'][-1]['position'], "Tracks: ",foo['release']['medium-list'][0]['track-list'][-1]['number']
+#result = musicbrainzngs.search_releases(artist=args.artist, release=args.title)
+def gather(artist,title):
+    result = musicbrainzngs.search_releases(artist=artist, release=title)
+    for item in result['release-list']:
+        if (item['artist-credit'][0]['artist']['name'] == artist and item['title'] == title):
+            actual = item['id']
+            foo = musicbrainzngs.get_release_by_id(actual,includes=['recordings','release-groups'])
+    print "Positions: ",foo['release']['medium-list'][0]['track-list'][-1]['position'], "Tracks: ",foo['release']['medium-list'][0]['track-list'][-1]['number']
+    #return foo['release']['medium-list'][0]['track-list'][-1]['number']
 
 
 
