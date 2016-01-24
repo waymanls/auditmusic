10eb98dd66f250c57d3385a85209689a632460fd
luckyleftie <wayman.smith@gmail.com>
2016-01-23 20:16:56 -0500

    Updated new.py and querymusicbrainz, at a working point for individual directories. Need to add handling for special characters

--- new.py
+++ new.py
@@ -6,6 +6,16 @@ import sys, parser, os, re
 import argparse
 import querymusicbrainz
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def main():
     parser = argparse.ArgumentParser(description='Music Scanner')
     parser.add_argument('-d','--directory', help='Directory', required=True)
@@ -21,7 +31,7 @@ def main():
         else:
             try:
                 # Separate out Artist Name from Album Title
-                em = re.search(r'^.*/(music|MUSIC|Music)/(.*)_-_(.*)/?',dirName)
+                em = re.search(r'^.*/(newmusic|music|MUSIC|Music)/(.*)_-_(.*)/?',dirName)
                 # Prune off extra "/". if group 2 contains a "/" character, don't print
                 if re.search(r'/',em.group(2)):
                     pass
@@ -29,7 +39,7 @@ def main():
                     #print em.group(1) ,"~~~", em.group(2)
                     for fname in fileList:
                         # Get actual music files, not other files
-                        if re.search(r'\.(flac|wav|mp3|m4a|mp4|wma)',fname):
+                        if re.search(r'\.(flac$|wav$|mp3$|m4a$|mp4$|wma$)',fname):
                             L.append(fname)
                             x = x+1
                     if x == 0:
@@ -39,10 +49,16 @@ def main():
                         # Print out total files contained in Album
                         #print x , "songs in", em.group(1) , em.group(2)
                         # Do you want to print this data to stdout or write it to a file? 
-                        print x , "songs in", dirName
-                        print em.group(2),em.group(3)
+                        group2 = re.sub(r"_",' ', em.group(2))
+                        group3 = re.sub(r"_",' ', em.group(3))
+                        if re.search(r'/',group3):
+                            group3 = group3.rstrip('/')
+                        #print group2,group3
                         # Function that compares my albums to musicBrainz goes here!
-                        querymusicbrainz.gather(em.group(2),em.group(3))
+                        foundtracks = querymusicbrainz.gather(group2,group3)
+                        if int(x) != int(foundtracks):
+                          print bcolors.WARNING + "You're missing some tracks bro!" + bcolors.ENDC
+                        print x , "songs in", dirName, foundtracks, "in MusicBrainz"
                     L = []
                     x = 0
             except AttributeError:
--- querymusicbrainz.py
+++ querymusicbrainz.py
@@ -3,6 +3,9 @@
 import musicbrainzngs
 import argparse
 import sys
+import re
+import json
+import io
 
 #parser = argparse.ArgumentParser(description='Audit Music collection')
 #parser.add_argument('-a','--artist', help='Artist Name', required=True)
@@ -16,15 +19,19 @@ musicbrainzngs.set_useragent(
 # Add getopts thing. So you can run it like this
 # querymusicbrainz.py -a <ARTIST> -t <ALBUM TITLE>
 #result = musicbrainzngs.search_releases(artist=args.artist, release=args.title)
+actual = []
 def gather(artist,title):
     result = musicbrainzngs.search_releases(artist=artist, release=title)
     for item in result['release-list']:
-        if (item['artist-credit'][0]['artist']['name'] == artist and item['title'] == title):
-            actual = item['id']
-            foo = musicbrainzngs.get_release_by_id(actual,includes=['recordings','release-groups'])
-    print "Positions: ",foo['release']['medium-list'][0]['track-list'][-1]['position'], "Tracks: ",foo['release']['medium-list'][0]['track-list'][-1]['number']
-    #return foo['release']['medium-list'][0]['track-list'][-1]['number']
-
-
-
+        #print json.dumps(item,sort_keys=True,indent=4, separators=(',', ': '))
+        if (re.search(artist, item['artist-credit'][0]['artist']['name'],flags=re.IGNORECASE) and re.search(artist, item['artist-credit-phrase'],flags=re.IGNORECASE) and re.search(title, item['title'],flags=re.IGNORECASE) and re.search('Album', item['release-group']['primary-type'])):
+        #if (item['artist-credit'][0]['artist']['name'] == artist and item['title'] == title):
+            actual.append(item['id'])
+            print actual[0]
+            foo = musicbrainzngs.get_release_by_id(actual[0],includes=['recordings','release-groups'])
+        #elif (item['title'] == title):
+        #    actual = item['id']
+        #    foo = musicbrainzngs.get_release_by_id(actual,includes=['recordings','release-groups'])
+    #print "Positions: ",foo['release']['medium-list'][0]['track-list'][-1]['position'], "Tracks: ",foo['release']['medium-list'][0]['track-list'][-1]['number']
+    return foo['release']['medium-list'][0]['track-list'][-1]['number']
 # vim:ts=4:expandtab:
